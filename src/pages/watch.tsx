28241f7f1016ce48fc40bb24985bdc4c7d1e3b68
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 12:49:59 +0700

    Update watch.tsx

--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -1,6 +1,6 @@
 import Nav from '@/components/nav/nav';
 import VideoSearchCard from '@/components/page/VideoSearchCard';
-import { IVideoInfo, VideoJSON, ChannelInfo } from '@/components/page/common'; // Pastikan ChannelInfo atau tipe yang sesuai dengan properti subscribers ada di sini
+import { IVideoInfo, VideoJSON } from '@/components/page/common';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { interFont } from '@/lib/constants';
 import { cn, fetcher, formatNumber, acronym } from '@/lib/utils';
@@ -13,21 +13,19 @@ export default function Watch() {
   const router = useRouter();
   const { v } = router.query;
 
-  const { data, error, isValidating } = useSWR<IVideoInfo>(() => {
+  const { data, error, isLoading } = useSWR<IVideoInfo>(() => {
     if (!v) {
       router.replace('/');
       return null;
     }
-    return `/api/video?id=${encodeURIComponent(
-      `https://www.youtube.com/watch?v=${v}`
-    )}`;
+    return `/api/video?id=${encodeURIComponent(v as string)}`;
   }, fetcher);
 
   if (error) return <div>Failed to load</div>;
-  if (!data || isValidating) return <div>Loading...</div>;
+  if (!data || isLoading) return <div>Loading...</div>;
 
-  // Menambahkan tipe data untuk channelInfo
-  const channelInfo = data.video.channel as ChannelInfo;
+  const video = data.video;
+  const relatedVideos = data.related;
 
   return (
     <>
@@ -35,23 +33,22 @@ export default function Watch() {
       <main className="px-6">
         <div className="flex flex-col lg:flex-row justify-between gap-4 my-4">
           <div className="flex-1">
-            <div className="aspect-w-16 aspect-h-9">
-              <iframe
-                src={`https://www.youtube.com/embed/${data.video.id}?autoplay=1&enablejsapi=1`}
-                className="border-none w-full h-full"
-                allowFullScreen
-              />
-            </div>
+            <iframe
+              src={`https://www.youtube.com/embed/${video.id}?autoplay=1&enablejsapi=1`}
+              className="aspect-video border-none w-full"
+              allowFullScreen
+            />
             <div>
               <div className="py-2 flex justify-between items-center flex-col lg:flex-row">
                 <div>
-                  <h1 className="text-xl font-semibold">{data.video.title}</h1>
+                  <h1 className="text-xl font-semibold">{video.title}</h1>
                 </div>
                 <div className="flex gap-2 items-center bg-secondary rounded-full p-3">
                   <div className="flex items-center gap-2 border-r dark:border-white/60 pr-4 cursor-pointer select-none">
                     <ThumbsUp className="h-5 w-5" />
-                    <span>{formatNumber(data.video.ratings.likes)}</span>
+                    <span>{formatNumber(video.ratings?.likes || 0)}</span>
                   </div>
+
                   <div>
                     <ThumbsDown className="h-5 w-5 cursor-pointer select-none" />
                   </div>
@@ -59,41 +56,39 @@ export default function Watch() {
               </div>
               <div className="flex gap-5 items-start">
                 <Avatar>
-                  <AvatarFallback>{acronym(channelInfo.name)}</AvatarFallback>
-                  <AvatarImage src={channelInfo.icon} />
+                  <AvatarFallback>{acronym(video.channel.name)}</AvatarFallback>
+                  <AvatarImage src={video.channel.icon.url} />
                 </Avatar>
                 <div>
-                  <h1 className="font-semibold">{channelInfo.name}</h1>
+                  <h1 className="font-semibold">{video.channel.name}</h1>
                   <p className="text-xs text-muted-foreground">
-                    {channelInfo.subscribers ? `${channelInfo.subscribers} subscribers` : 'N/A'}
+                    {video.channel.subscribers || 'N/A'} subscribers
                   </p>
                 </div>
               </div>
               <div className="bg-secondary my-5 p-3 rounded-lg">
                 <div className="text-sm font-semibold gap-3 flex">
-                  <span>{formatNumber(data.video.views)} views</span>
-                  <span>{data.video.uploadedAt}</span>
+                  <span>{video.views.toLocaleString()} views</span>
+                  <span>{video.uploadedAt}</span>
                 </div>
                 <div className="overflow-auto">
                   <pre className={cn(interFont.className, 'break-words text-sm')}>
-                    {data.video.description}
+                    {video.description || 'N/A'}
                   </pre>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 w-full lg:w-1/4">
             <h1 className="text-sm font-semibold">Related Videos</h1>
-            {data?.related?.map((video) => (
-              <Link href={`/watch?v=${video.id}`} key={video.id}>
-                <a>
-                  <VideoSearchCard key={video.id} video={video} small />
-                </a>
-              </Link>
-            )) || (
-              <p className="text-xs text-muted-foreground">
-                No related videos available.
-              </p>
+            {relatedVideos.length > 0 ? (
+              relatedVideos.map((relatedVideo) => (
+                <Link href={`/watch?v=${relatedVideo.id}`} key={relatedVideo.id}>
+                  <VideoSearchCard key={relatedVideo.id} video={relatedVideo} small />
+                </Link>
+              ))
+            ) : (
+              <p className="text-xs text-muted-foreground">No data available.</p>
             )}
           </div>
         </div>
