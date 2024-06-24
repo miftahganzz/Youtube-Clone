a4cb7387d8c6520ed4d93ad9f70763f19c0d5712
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 11:52:53 +0700

    Update home.ts

--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -2,6 +2,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { YouTube } from "youtube-sr";
 
 export default async function Home(req: NextApiRequest, res: NextApiResponse) {
-  const videos = await YouTube.homepage();
-  return res.json(videos.map((video) => video.toJSON()));
+  const queries = [
+    "trending music",
+    "trending gaming",
+    "tranding music 2024",
+    "sad vibes",
+    "random gaming",
+    "random music",
+    "random meme",
+    "random game"
+  ];
+
+  const results = await Promise.all(
+    queries.map(async (query) => {
+      const videos = await YouTube.search(query); 
+      return videos.map((video) => video.toJSON());
+    })
+  );
+
+  return res.json(results);
 }
