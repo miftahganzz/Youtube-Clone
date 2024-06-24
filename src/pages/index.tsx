4a00e62b4dfa4a41917922505553ac61282ffbcc
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 12:10:06 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,33 @@
-import useSWR from 'swr';
-import Link from 'next/link';
 import Nav from '@/components/nav/nav';
 import VideoCard from '@/components/page/VideoCard';
 import { VideoJSON } from '@/components/page/common';
 import { fetcher } from '@/lib/utils';
+import Link from 'next/link';
+import useSWR from 'swr';
 
 export default function Home() {
-  const { data: results, error, isValidating } = useSWR<any[]>('/api/home', fetcher);
+  const {
+    data: videos,
+    error,
+    isLoading,
+  } = useSWR<VideoJSON[]>('/api/home', fetcher);
 
   if (error) return <div>Failed to load</div>;
-  if (!results) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
       <Nav />
       <main className="container">
-        {results.map((videos, index) => (
-          <div key={index} className="my-5">
-            <h2 className="text-xl font-bold mb-3">Query {index + 1}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row gap-5">
-              {videos.map((video: VideoJSON) => (
-                <Link key={video.id} href={`/watch?v=${video.id}`}>
-                  <a>
-                    <VideoCard key={video.id} video={video} />
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row gap-5 my-5">
+          {videos!.map((video) => {
+            return (
+              <Link key={video.id} href={/watch?v=${video.id}}>
+                <VideoCard key={video.id} video={video} />
+              </Link>
+            );
+          })}
+        </div>
       </main>
     </>
   );
