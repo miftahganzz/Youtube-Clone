b3c027fe948e88d506ca5181a90e52c269c96ab5
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 12:04:14 +0700

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
