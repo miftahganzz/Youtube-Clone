d7041d58d489e9f9069d38b74fef01b240e27682
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 13:06:25 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,19 @@ import { VideoJSON } from '@/components/page/common';
 import { fetcher } from '@/lib/utils';
 
 export default function Home() {
-  const { data: categories, error } = useSWR<{ [key: string]: VideoJSON[] }>('/api/home', fetcher);
+  const { data: results, error } = useSWR<any[]>('/api/home', fetcher);
 
   if (error) return <div>Failed to load</div>;
-  if (!categories) return <div>Loading...</div>;
+  if (!results) return <div>Loading...</div>;
 
   return (
     <>
       <Nav />
       <main className="container">
-        {Object.keys(categories).map((category, index) => (
+        {results.map((videos, index) => (
           <div key={index} className="my-5">
-            <h2 className="text-xl font-semibold">{category}</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row gap-5">
-              {categories[category].map((video: VideoJSON) => (
+              {videos.map((video: VideoJSON) => (
                 <Link key={video.id} href={`/watch?v=${video.id}`}>
                   <a>
                     <VideoCard key={video.id} video={video} />
