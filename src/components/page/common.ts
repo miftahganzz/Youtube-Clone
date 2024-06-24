292f3e396d5bb6dd49795c3bbba56a4473d5ce6a
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 12:46:38 +0700

    Update common.ts

--- src/components/page/common.ts
+++ src/components/page/common.ts
@@ -1,7 +1,12 @@
-import type { Video } from 'youtube-sr';
+import type { Video, Channel } from 'youtube-sr';
 
 export type VideoJSON = ReturnType<Video['toJSON']>;
+
 export interface IVideoInfo {
   video: VideoJSON;
   related: VideoJSON[];
+}
+
+export interface ChannelInfo extends Channel {
+  subscribers?: string; 
 }
