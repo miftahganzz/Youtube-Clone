2dc94923b3512ea3f611ae524317745c52496dc6
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-06-24 13:19:06 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,11 @@ import useSWR from 'swr';
 import Link from 'next/link';
 import Nav from '@/components/nav/nav';
 import VideoCard from '@/components/page/VideoCard';
+import { VideoJSON } from '@/components/page/common';
 import { fetcher } from '@/lib/utils';
 
-interface VideoJSON {
-  id: string;
-  title: string;
-  thumbnail: { url: string };
-}
-
-interface QueryResult {
-  query: string;
-  videos: VideoJSON[];
-}
-
 export default function Home() {
-  const { data: results, error } = useSWR<QueryResult[]>('/api/home', fetcher);
+  const { data: results, error } = useSWR<any[]>('/api/home', fetcher);
 
   if (error) return <div>Failed to load</div>;
   if (!results) return <div>Loading...</div>;
@@ -25,11 +15,10 @@ export default function Home() {
     <>
       <Nav />
       <main className="container">
-        {results.map((result, index) => (
+        {results.map((videos, index) => (
           <div key={index} className="my-5">
-            <h2 className="text-xl font-bold mb-3">Category: {result.query}</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row gap-5">
-              {result.videos.map((video) => (
+              {videos.map((video: VideoJSON) => (
                 <Link key={video.id} href={`/watch?v=${video.id}`}>
                   <a>
                     <VideoCard key={video.id} video={video} />
